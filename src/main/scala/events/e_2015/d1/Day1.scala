7efe0a73c951c7c2b610340f683eb270ed69644f
Ostap Sulyk <60716383+Ostap-Sulyk@users.noreply.github.com>
2023-04-13 17:15:33 -0400

    did some minor refactoring

--- src/main/scala/events/e_2015/d1/Day1.scala
+++ src/main/scala/events/e_2015/d1/Day1.scala
@@ -4,31 +4,29 @@ import events.Challenge
 
 import scala.io.BufferedSource
 import scala.io.Source.fromFile
-import scala.util.control.Breaks.{break, breakable}
 
 class Day1 extends Challenge {
   private val data: BufferedSource = fromFile("src/main/scala/events/e_2015/d1/input.txt")
-  private var floors: List[Int] = data.mkString.map {
+  private var floor = 0
+  private var index: Option[Int] = None
+
+  private val floors: List[Int] = data.mkString.map {
     case '(' => 1
     case ')' => -1
   }.toList
-  private var floor = 0
-  private var index: Option[Int] = None
 
   private def task1: Int = floors.sum
 
   private def task2: Int = {
-    breakable {
-      for ((x, y) <- floors.zipWithIndex) {
-        floor += x
-        if (floor == -1) {
-          index = Some(y + 1)
-          break
-        }
+    for (i <- floors.indices) {
+      floor += floors(i)
+      if (index.isEmpty) {
+        if (floor == -1) index = Some(i + 1)
       }
     }
     index.get
   }
+
 
   override def solve(): String =
     s"Task 1: $task1\tTask2: $task2"
