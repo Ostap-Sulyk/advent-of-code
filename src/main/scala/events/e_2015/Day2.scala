2ef77be867eecd9807e35035f620b8fe626631af
Ostap Sulyk <60716383+Ostap-Sulyk@users.noreply.github.com>
2023-04-13 19:06:30 -0400

    Solved day 2 task 2

--- src/main/scala/events/e_2015/Day2.scala
+++ src/main/scala/events/e_2015/Day2.scala
@@ -1,26 +1,33 @@
 package events.e_2015
 
+import events.printNicely
+import events.Challenge
+
 import scala.io.Source.fromFile
 
-private class Box(val l: Int, val w: Int, val h: Int) {
-  def surfaceArea: Int = (2 * l * w) + (2 * w * h) + (2 * h * l)
 
-  def smallestSide: Int = List(l * w, w * h, h * l).min
+class Day2 extends Challenge {
+  private class Box(val l: Int, val w: Int, val h: Int) {
+    def surfaceArea: Int = (2 * l * w) + (2 * w * h) + (2 * h * l)
 
-  override def toString: String = s"Box($l, $w, $h)"
-}
+    def smallestSide: Int = List(l * w, w * h, h * l).min
 
-def parseLineToBox(line: String): Box = {
-  val dims = line.split("x").map(_.toInt)
-  new Box(dims(0), dims(1), dims(2))
-}
+    def ribbon: Int = List(2 * l + 2 * w, 2 * w + 2 * h, 2 * h + 2 * l).min + l * w * h
 
-object Day2 extends App {
-  val data = fromFile("src/main/scala/events/e_2015/inputs/day2.txt")
-  val dataAsList: List[Box] = data.getLines().map(parseLineToBox).toList
+  }
 
+  private def parseLineToBox(line: String): Box = {
+    val dims = line.split("x").map(_.toInt)
+    new Box(dims(0), dims(1), dims(2))
+  }
 
-  def task1: Int = dataAsList.map(x => x.surfaceArea + x.smallestSide).sum
+  private val data = fromFile("src/main/scala/events/e_2015/inputs/day2.txt")
+  private val dataAsList: List[Box] = data.getLines().map(parseLineToBox).toList
 
+  private def task1: Int = dataAsList.map(x => x.surfaceArea + x.smallestSide).sum
+
+  private def task2: Int = dataAsList.map(x => x.ribbon).sum
+
+  def getAnswer: String = printNicely(this.getClass.getSimpleName, task1, task2)
 
 }
