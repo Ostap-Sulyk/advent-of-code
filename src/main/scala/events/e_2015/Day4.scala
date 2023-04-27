ed454578b17ca2f04a79aacd07275f75cba4d5f0
Ostap Sulyk <60716383+Ostap-Sulyk@users.noreply.github.com>
2023-04-27 00:43:26 -0400

    Day4 Task1 ✅

---
+++ src/main/scala/events/e_2015/Day4.scala
@@ -0,0 +1,26 @@
+package events.e_2015
+
+import events.{Challenge, printNicely}
+
+import java.security.MessageDigest
+import scala.concurrent.*
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.*
+
+class Day4 extends Challenge{
+  val md = MessageDigest.getInstance("MD5")
+  val secretKey = "iwrupvqb"
+  val prefix1 = "0" * 5
+  val prefix2 = "0" * 6
+
+  def hash(input: String): String = {
+    md.reset()
+    md.update(input.getBytes("UTF-8"))
+    md.digest().map("%02x".format(_)).mkString
+  }
+
+  def task1: Int = LazyList.from(1).find(i => hash(secretKey + i).startsWith(prefix1)).get
+
+
+}
+
