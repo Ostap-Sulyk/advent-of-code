c852c144de49ae898ba67a2a0001cf7d983717af
Ostap Sulyk <60716383+Ostap-Sulyk@users.noreply.github.com>
2023-04-27 00:43:50 -0400

    Day4 Task2 ✅

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,17 +1,13 @@
 import events.e_2015.*
 
-
 @main
 def main(): Unit = {
   val days = List(
     new Day1,
     new Day2,
-    new Day3
+    new Day3,
+    new Day4,
   )
 
-  days.foreach { day =>
-    println(day.getAnswer)
-  }
-
-
+  days.foreach(day => println(day.getAnswer))
 }
